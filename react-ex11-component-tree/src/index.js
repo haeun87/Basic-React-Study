993dfb2ae6ddbefb461f9c7cb0f7b2cb5873e157
서해은 <husuh@diquest.com>
2021-02-25 18:09:04 +0900

    20210225 학습분 커밋

---
+++ react-ex11-component-tree/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+window.React = React;
+
+ReactDOM.render(
+    <App />,
+  document.getElementById('root')
+);
+
